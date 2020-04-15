ddf20067c4de8d495a95d7d57f6d11128e97f964
Archerhino <archerhino@users.noreply.github.com>
2020-04-15 05:04:38 -0500

    RestTemplate - Post

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -2,7 +2,9 @@ package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entity.CommonResult;
 import com.atguigu.springcloud.entity.Payment;
+import com.atguigu.springcloud.enumeration.ResponseStatus;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,8 +25,31 @@ public class OrderController {
     public CommonResult<Payment> create(Payment payment){
         return restTemplate.postForObject(PAYMENT_URL + "/payment", payment, CommonResult.class);
     }
+
+    @GetMapping(value = "/consumer/payment/entity")
+    public CommonResult<Payment> createEntity(Payment payment){
+        ResponseEntity<CommonResult> responseEntity = restTemplate.postForEntity(PAYMENT_URL + "/payment", payment, CommonResult.class);
+        if(responseEntity.getStatusCode().is2xxSuccessful()){
+            log.info("Status Code: {}, Header: {}", responseEntity.getStatusCodeValue(), responseEntity.getHeaders());
+            return responseEntity.getBody();
+        }else{
+            return new CommonResult<>(ResponseStatus.FAIL, "fail");
+        }
+    }
+
     @GetMapping(value = "/consumer/payment/{id}")
     public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
         return restTemplate.getForObject(PAYMENT_URL + "/payment/" + id, CommonResult.class);
+    }
+
+    @GetMapping(value = "/consumer/payment/entity/{id}")
+    public CommonResult<Payment> getPaymentEntityById(@PathVariable("id") Long id){
+        ResponseEntity<CommonResult> responseEntity = restTemplate.getForEntity(PAYMENT_URL + "/payment/" + id, CommonResult.class);
+        if(responseEntity.getStatusCode().is2xxSuccessful()){
+            log.info("Status Code: {}, Header: {}", responseEntity.getStatusCodeValue(), responseEntity.getHeaders());
+            return responseEntity.getBody();
+        }else{
+            return new CommonResult<>(ResponseStatus.FAIL,"fail");
+        }
     }
 }
