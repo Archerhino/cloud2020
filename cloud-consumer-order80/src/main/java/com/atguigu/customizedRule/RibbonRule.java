b9c0b81ebacc731c9ed9e7a5a5a337a8a6db3c9f
Archerhino <archerhino@users.noreply.github.com>
2020-04-15 15:14:35 -0500

    Ribbon change LoadBlancer

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/customizedRule/RibbonRule.java
@@ -0,0 +1,14 @@
+package com.atguigu.customizedRule;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RibbonRule {
+    @Bean
+    public IRule iRule(){
+        return new RandomRule();
+    }
+}
