ca1521829a5a47549aea4669e90c1709b1a5ba8f
Archerhino <archerhino@users.noreply.github.com>
2020-04-22 15:50:07 -0500

    Hystrix circuit breaker

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -3,4 +3,5 @@ package com.atguigu.springcloud.service;
 public interface PaymentService {
     String paymentInfo(Integer id);
     String paymentInfoMockingTimeout(Integer id);
+    String paymentCircuitBreaker(Integer id);
 }
--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
@@ -4,7 +4,9 @@ import com.atguigu.springcloud.service.PaymentService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -33,5 +35,24 @@ public class PaymentServiceImpl implements PaymentService {
 
     public String paymentInfoTimeoutHandler(Integer id){
         return Thread.currentThread().getName() + " System is busy, please retry...id: " + id;
+    }
+
+    @Override
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreakerFallbackMethod", commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"), // milliseconds after tripping circuit before allowing retry
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60")
+    })
+    public String paymentCircuitBreaker(Integer id){
+        if(id < 0){
+            throw new RuntimeException("*****id can not be negative!");
+        }
+        String serial = UUID.randomUUID().toString();
+        return Thread.currentThread().getName() + "\t Invoke success, serial#: " + serial;
+    }
+
+    public String paymentCircuitBreakerFallbackMethod(Integer id){
+        return "Invoke fail, id can not be negative! Please retry in 20 seconds, id: " + id;
     }
 }
